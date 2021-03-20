ede4a8e081cc6af4a03657caae58297cb6c9b1e2
Quinn T <Quinn.GitHub@gmail.com>
2021-03-20 03:53:18 -0500

    rename spec.ts to test.ts

--- src/custom/errors/test.ts
+++ src/custom/errors/test.ts
@@ -32,20 +32,6 @@ err2.child = err3
 err3.child = err4
 err4.child = err5
 
-
-
-
-describe("this test", () => {
-    it("will fail", () => {
-        expect(1).toBe(1)
-    })
-})
-
-
-
-
-
-
 // TODO make the SourcePoint and SourceLine stuff store an ID of the source they are referencing
 // TODO and allow error objects to look up sources by id somehow
 var almost_empty=new SourceText('1\n2',"almost-empty.txt");
